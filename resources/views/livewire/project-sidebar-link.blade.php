5c4de4e86826780b596f26ea634822813d81e395
Jamie Henrickson <53891976+Readyforeal@users.noreply.github.com>
2023-12-19 22:43:48 -0600

    Update project-sidebar-link.blade.php

--- resources/views/livewire/project-sidebar-link.blade.php
+++ resources/views/livewire/project-sidebar-link.blade.php
@@ -1,4 +1,4 @@
-<a href="{{ $link }}" class="flex w-full py-2 text-left rounded-md opacity-70 group cursor-pointer hover:opacity-100 hover:bg-blue-500/20 transition ease-in-out">
+<a href="{{ $link }}" class="flex w-full py-2 text-left rounded-md group cursor-pointer {{ $highlighted ? 'opacity-100' : 'opacity-70' }} hover:opacity-100 hover:bg-blue-500/20 transition ease-in-out">
     <span class="flex group-hover:translate-x-3 transition ease-in-out {{ $highlighted ? 'text-blue-500' : '' }}">
         <i class="{{ $icon }} mt-1 mr-3 group-hover:text-blue-500 transition ease-in-out"></i>
         <p class="font-semibold">{{ $label }}</p>
