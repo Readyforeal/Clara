077ca79978454ec13049809dc13ce4729ec8a845
Jamie Henrickson <53891976+Readyforeal@users.noreply.github.com>
2024-01-01 17:01:26 -0600

    UI Changes

--- resources/views/components/button-link.blade.php
+++ resources/views/components/button-link.blade.php
@@ -1,7 +1,7 @@
 @props(['icon' => '', 'url' => ''])
 
 <a {{ $attributes->merge(['href' => $url, 'class' => 'inline-flex items-center px-6 py-2 font-semibold text-xs border rounded-xl transition ease-in-out
-    bg-black hover:bg-gray-800 border-transparent text-gray-300 hover:text-white']) }}>
+    bg-black hover:bg-gray-800 border-transparent text-gray-100 hover:text-white']) }}>
     @isset($slot)
         {{ $slot }}
     @endisset
